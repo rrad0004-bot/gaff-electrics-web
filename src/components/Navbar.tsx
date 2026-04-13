7098c2688a57de94336aa4bb1107b84360a69afe
Rahul Radhakrishnan <rrad0004@student.monash.edu>
2026-04-13 21:46:26 +1000

    2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,37 @@
 'use client';
+
 import { useState } from 'react';
 import Link from 'next/link';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <nav className={styles.navbar}>
       <div className={`container ${styles.navContainer}`}>
-        <Link href="/" className={styles.logo}>
+
+        <Link href="/" className={styles.logo} onClick={closeMenu}>
           GAFF<span>Electrics</span>
         </Link>
 
         <div className={`${styles.navLinks} ${menuOpen ? styles.showMenu : ''}`}>
-          <Link href="/">Home</Link>
-          <Link href="/about">About Us</Link>
-          <Link href="/services">Services</Link>
-          <Link href="/warranty">Warranty</Link>
-          <Link href="/faq">FAQ</Link>
-          <Link href="/contact">Contact</Link>
+          <Link href="/" onClick={closeMenu}>Home</Link>
+          <Link href="/about" onClick={closeMenu}>About Us</Link>
+          <Link href="/services" onClick={closeMenu}>Services</Link>
+          <Link href="/warranty" onClick={closeMenu}>Warranty</Link>
+          <Link href="/faq" onClick={closeMenu}>FAQ</Link>
+          <Link href="/contact" onClick={closeMenu}>Contact</Link>
         </div>
 
         <div className={styles.navActions}>
           <a href="tel:18005550199" className={styles.phoneLink}>
             📞 1800-555-0199
           </a>
-          <Link href="/quote" className="btn-primary">
+
+          <Link href="/quote" className="btn-primary" onClick={closeMenu}>
             Request Quote
           </Link>
         </div>
@@ -36,6 +42,7 @@ export default function Navbar() {
         >
           ☰
         </button>
+
       </div>
     </nav>
   );
